f6b501f9005eb564deb3bdb0837db5415c393ec7
tsgo3 <39001182@parisnanterre.fr>
2022-10-17 17:28:37 +0200

    Question 2 et 3 test

--- src/Exo3/Main.java
+++ src/Exo3/Main.java
@@ -4,21 +4,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        final Arbre<Entier> f1 = new Feuille<>(1);
-        final Arbre<Entier> f2 = new Feuille<>(2);
+        final Arbre<Entier> f1 = new Feuille<>(new Entier(1));
+        final Arbre<Entier> f2 = new Feuille<>(new Entier(2));
         final Arbre<Entier> n1 = new Noeud<>(List.of(f1,f2));
-        final Arbre<Entier> f3 = new Feuille<>(3);
+        final Arbre<Entier> f3 = new Feuille<>(new Entier(3));
         final Arbre<Entier> n2 = new Noeud<>(List.of(n1,f3));
 
-        System.out.println(n2.contient(4));
-        System.out.println(n2.contient(1));
+        System.out.println(n2.contient(new Entier(4)));
+        System.out.println(n2.contient(new Entier (1)));
         System.out.println(n2.taille());
         System.out.println(n2.valeurs());
 
         System.out.println(f2.valeurs());
         System.out.println(f2.taille());
-        System.out.println(f2.contient(3));
-        System.out.println(f2.contient(2));
+        System.out.println(f2.contient(new Entier(3)));
+        System.out.println(f2.contient(new Entier(2)));
 
         /*final Arbre<Entier> f4 = new Feuille<>(1);*/
     }
